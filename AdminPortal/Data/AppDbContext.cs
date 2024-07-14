ec703a297500fca517b7d8e2c71e7c9490189767
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-07-14 03:54:29 -1200

    Add project files.

---
+++ AdminPortal/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminPortal.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) :base(options) 
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
