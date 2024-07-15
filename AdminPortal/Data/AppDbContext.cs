99fa483fe0688bb81b449a7a52cfb7d63524abee
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-07-15 06:01:19 -1200

    Add to database

--- AdminPortal/Data/AppDbContext.cs
+++ AdminPortal/Data/AppDbContext.cs
@@ -14,5 +14,72 @@ namespace AdminPortal.Data
         public DbSet<Difficulty> Difficulties { get; set; }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Walk> Walks { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Seed data for Difficulties
+            //Easy, Medium, Hard
+
+            var difficulties = new List<Difficulty>()
+            {
+                new Difficulty()
+                {
+                    Id = Guid.Parse("66c1acdb-cafc-44e4-be04-b8d9162101e2"),
+                    Name = "Easy"
+                },
+                 new Difficulty()
+                {
+                    Id = Guid.Parse("2e0eaf1b-b7ef-42a7-a914-9c1da89ebff3"),
+                    Name = "Medium"
+                },
+                  new Difficulty()
+                {
+                    Id = Guid.Parse("26636693-5e7d-470d-ab08-0006b1abacfc"),
+                    Name = "Hard"
+                }
+            };
+
+            //seed difficulties to the database
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+
+            //seed data for Regions
+            var regions = new List<Region>()
+            {
+                new Region()
+                {
+                    Id = Guid.Parse("598403e6-88c6-40a6-a51d-1abcd1e23dc3"),
+                    Name = "Ahmed Alzabr",
+                    Code = "AA",
+                    RegionImageUrl = "https://images.pexels.com/photos/26082992/pexels-photo-26082992/free-photo-of-alberoxalbero.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region()
+                {
+                    Id = Guid.Parse("b0d641f2-f07d-45fe-afc0-1a84eca538c3"),
+                    Name = "Ahmed Saleh",
+                    Code = "AS",
+                    RegionImageUrl = "https://images.pexels.com/photos/26082992/pexels-photo-26082992/free-photo-of-alberoxalbero.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region()
+                {
+                    Id = Guid.Parse("2a1d2fad-3446-4796-8524-5fbbea4c15e0"),
+                    Name = "Mohammed Zaid",
+                    Code = "MZ",
+                    RegionImageUrl = "https://images.pexels.com/photos/26082992/pexels-photo-26082992/free-photo-of-alberoxalbero.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region()
+                {
+                    Id = Guid.Parse("9c10ca47-6534-4557-b641-67d45ac5758b"),
+                    Name = "Samerh Ali",
+                    Code = "SA",
+                    RegionImageUrl = "https://images.pexels.com/photos/26082992/pexels-photo-26082992/free-photo-of-alberoxalbero.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                }
+            };
+
+            modelBuilder.Entity<Region>().HasData(regions);
+        }
     }
 }
---
+++ AdminPortal/Migrations/20240715175556_dificultRegion.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AdminPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class dificultRegion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("26636693-5e7d-470d-ab08-0006b1abacfc"), "Hard" },
+                    { new Guid("2e0eaf1b-b7ef-42a7-a914-9c1da89ebff3"), "Medium" },
+                    { new Guid("66c1acdb-cafc-44e4-be04-b8d9162101e2"), "Easy" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("2a1d2fad-3446-4796-8524-5fbbea4c15e0"), "MZ", "Mohammed Zaid", "https://images.pexels.com/photos/26082992/pexels-photo-26082992/free-photo-of-alberoxalbero.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+                    { new Guid("598403e6-88c6-40a6-a51d-1abcd1e23dc3"), "AA", "Ahmed Alzabr", "https://images.pexels.com/photos/26082992/pexels-photo-26082992/free-photo-of-alberoxalbero.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+                    { new Guid("9c10ca47-6534-4557-b641-67d45ac5758b"), "SA", "Samerh Ali", "https://images.pexels.com/photos/26082992/pexels-photo-26082992/free-photo-of-alberoxalbero.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+                    { new Guid("b0d641f2-f07d-45fe-afc0-1a84eca538c3"), "AS", "Ahmed Saleh", "https://images.pexels.com/photos/26082992/pexels-photo-26082992/free-photo-of-alberoxalbero.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("26636693-5e7d-470d-ab08-0006b1abacfc"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("2e0eaf1b-b7ef-42a7-a914-9c1da89ebff3"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("66c1acdb-cafc-44e4-be04-b8d9162101e2"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("2a1d2fad-3446-4796-8524-5fbbea4c15e0"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("598403e6-88c6-40a6-a51d-1abcd1e23dc3"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("9c10ca47-6534-4557-b641-67d45ac5758b"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("b0d641f2-f07d-45fe-afc0-1a84eca538c3"));
+        }
+    }
+}
