a99cb8ffa7a5b5195a6732e6aa1f234b1efe3134
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-07-23 13:12:17 -1200

    UI

--- AdminPortalUI/Models/AddRegionViewModel.cs
+++ AdminPortalUI/Models/AddRegionViewModel.cs
@@ -4,6 +4,6 @@
     {
         public string Code { get; set; }
         public string Name { get; set; }
-        public string ImageUrl { get; set; }
+        public string RegionImageUrl { get; set; }
     }
 }
